5489603524be4a295f992341c637018770a1b018
Jayant Kumar <jaykrs@gmail.com>
2023-07-03 23:29:51 +0530

    Update database.js

--- config/database.js
+++ config/database.js
@@ -2,10 +2,10 @@ module.exports = ({ env }) => ({
   connection: {
     client: 'mysql',
     connection: {
-      host: env('DATABASE_HOST', 'localhost'),
+      host: env('DATABASE_HOST', 'https://db4free.net'),
       port: env.int('DATABASE_PORT', 3306),
       database: env('DATABASE_NAME', 'mylearning'),
-      user: env('DATABASE_USERNAME', 'root'),
+      user: env('DATABASE_USERNAME', 'jaykrs'),
       password: env('DATABASE_PASSWORD', 'Welcome@0'),
       ssl: env.bool('DATABASE_SSL', false)
     },
