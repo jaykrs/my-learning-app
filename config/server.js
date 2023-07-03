3143f62a61caaf826cf3fed505f876dad730c5ef
Jayant Kumar <jaykrs@gmail.com>
2023-07-04 00:55:50 +0530

    Update server.js

--- config/server.js
+++ config/server.js
@@ -1,6 +1,7 @@
 module.exports = ({ env }) => ({
   host: env('HOST', '0.0.0.0'),
   port: env.int('PORT', 1337),
+  url: 'https://jaykrs.onrender.com',
   app: {
     keys: env.array('APP_KEYS'),
   },
