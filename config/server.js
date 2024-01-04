01e1aa83fc8bb37a4549b51331fe23eae35fe7b8
jaykrs <jaykrs@gmail.com>
2024-01-04 18:02:55 +0530

    port

--- config/server.js
+++ config/server.js
@@ -1,7 +1,7 @@
 module.exports = ({ env }) => ({
   host: env('HOST', '0.0.0.0'),
-  port: env.int('PORT', 1337),
-  url: 'http://localhost:1337',
+  port: env.int('PORT', 80),
+  url: 'http://localhost:80',
   app: {
     keys: env.array('APP_KEYS'),
   },
